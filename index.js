c97616355cd24eb4a069f2d11b6ae0a8c01817cd
Jai <jaijai.stell@gmail.com>
2023-03-21 14:55:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// jais code
+function changeText() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  changeText();
+});
+
+//trying to do the arrow function
+//document.addEventListener('DOMContentLoaded', ()=>{
+//    document.getElementById('text').textContent = "This is really cool!";
+//})
